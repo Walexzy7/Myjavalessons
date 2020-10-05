96d97642e7b7473e0a4a01c6fa15281341ca6215
= <=>
2020-10-05 12:44:43 +0100

    updated rectangle classes

--- Rectangle.java
+++ Rectangle.java
@@ -1,31 +1,31 @@
 public class Rectangle{
-  private double lenght;
+  private double length;
   private double width;
 
 
     Rectangle(){
  }
     
-    Rectangle(double lenght){
-        this.lenght = lenght;
+    Rectangle(double length){
+        this.length = length;
     }
    
-    Rectangle(double lenght, double width){
-        this.lenght = lenght;
+    Rectangle(double length, double width){
+        this.length = length;
         this.width = width;
     }
 
    
-    public void setLenght(double lenght){
-        this.lenght = lenght;
+    public void setLength(double length){
+        this.length = length;
     }
 
     public void setWidth(double width){
         this.width = width;
     }
 
-    public double getLenght(){
-        return this.lenght;
+    public double getLength(){
+        return this.length;
     }
 
     public double getWidth(){
@@ -33,6 +33,30 @@ public class Rectangle{
     }
 
     public double areaOfRectangle(){
-        return lenght*width;
+        /**
+         * Use this method when no external value
+         * is provided.
+         */
+        return this.length * this.width;
+    }
+
+
+    public double areaOfRectangle(int width){
+        /**
+         * Use this method if only the width of the Rectangle is 
+         * provided. This will return area based on original length of 
+         * rectangle
+         */
+        return this.length * width;
+    }
+
+
+    public double areaOfRectangle(int length, int width){
+        /**
+         * Use this method if both the length and width of the Rectangle are 
+         * provided. This will return area based on newly supplied values for
+         * length and width of rectangle
+         */
+        return length * width;
     }
 }
